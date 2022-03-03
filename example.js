f10f9f2022f601d174b0f3eea93df42c182ad37e
Bryce <73015472+BryceTrader@users.noreply.github.com>
2022-03-03 01:03:16 +0000

    doubleIt example

--- example.js
+++ example.js
@@ -1,36 +1,26 @@
-const FILENAME = 'test'
+const FILENAME = 'doubleIt'
 
 const EXAMPLES = `入力例1
+1
+2
 3
-10
+4
 5
-8
-3
-1 3 5
-3 2 3
-2 1 10
 出力例1
-13
-0
+2
+4
+6
+8
 10
 入力例2
-5
-51
-49
-15
-61
-40
-5
-3 1 5
-5 2 14
-4 3 79
-3 5 31
-4 3 51
-出力例2
-56
-63
-40
 0
-57`
+-1
+-10
+0.5
+出力例2
+0
+-2
+-20
+1`
 
 module.exports = [FILENAME, EXAMPLES]
